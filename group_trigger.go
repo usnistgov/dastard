91ddd9cd898d2279ed19f7080ea87f71cd366c55
Joe Fowler <drjoefowler@gmail.com>
2017-10-27 11:55:39 -0600

    Added mutex to guard the TriggerBroker connections

--- group_trigger.go
+++ group_trigger.go
@@ -1,6 +1,9 @@
 package dastard
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type receiverSet map[int]bool
 
@@ -9,6 +12,10 @@ type receiverSet map[int]bool
 type TriggerBroker struct {
 	nchannels       int
 	sources         []map[int]bool
+	PrimaryTrigs    chan []int64
+	SecondaryTrigs  chan []int64
+	latestPrimaries [][]int
+	lock            sync.Mutex
 }
 
 // NewTriggerBroker creates a new TriggerBroker object for nchan channels to share group triggers.
@@ -28,7 +35,9 @@ func (broker *TriggerBroker) AddConnection(source, receiver int) error {
 		return fmt.Errorf("Could not add channel %d as a group receiver (nchannels=%d)",
 			receiver, broker.nchannels)
 	}
+	broker.lock.Lock()
 	broker.sources[receiver][source] = true
+	broker.lock.Unlock()
 	return nil
 }
 
@@ -38,7 +47,9 @@ func (broker *TriggerBroker) DeleteConnection(source, receiver int) error {
 		return fmt.Errorf("Could not remove channel %d as a group receiver (nchannels=%d)",
 			receiver, broker.nchannels)
 	}
+	broker.lock.Lock()
 	delete(broker.sources[receiver], source)
+	broker.lock.Unlock()
 	return nil
 }
 
@@ -47,7 +58,9 @@ func (broker *TriggerBroker) isConnected(source, receiver int) bool {
 	if receiver < 0 || receiver >= broker.nchannels {
 		return false
 	}
+	broker.lock.Lock()
 	_, ok := broker.sources[receiver][source]
+	broker.lock.Unlock()
 	return ok
 }
 
@@ -56,5 +69,19 @@ func (broker *TriggerBroker) Connections(receiver int) map[int]bool {
 	if receiver < 0 || receiver >= broker.nchannels {
 		return nil
 	}
-	return broker.sources[receiver]
+	broker.lock.Lock()
+	sources := broker.sources[receiver]
+	broker.lock.Unlock()
+	return sources
+}
+
+// Run runs in a goroutine to broker trigger frame #s from sources to receivers
+func (broker *TriggerBroker) Run(abort <-chan struct{}) {
+	for {
+		// get data from all PrimaryTrigs channels
+
+		// send reponse to all SecondaryTrigs channels
+
+		// check for close... how?
+	}
 }
