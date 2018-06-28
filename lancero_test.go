ce6621b6561420298c7bc66880ead54172a2ebc1
ggggggggg <galen.oneil@nist.gov>
2018-06-28 15:45:01 -0600

    better way to make -race work
    I increase lsync in the NoHardwareSource test, but to get that to work I have to remove the minimum buffer size in sampleCard. Lets wait till thats acutally a problem.

--- lancero_test.go
+++ lancero_test.go
@@ -2,6 +2,9 @@ package dastard
 
 import (
 	"testing"
+	"time"
+
+	"github.com/usnistgov/dastard/lancero"
 )
 
 // TestChannelOrder checks the map from channel to readout numbering
@@ -67,6 +70,62 @@ func TestChannelOrder(t *testing.T) {
 			t.Errorf("roundint(%f)=%d, want %d", f, roundint(f), es[i])
 		}
 	}
+}
+
+func TestNoHardwareSource(t *testing.T) {
+	var ncolsSet, nrowsSet, linePeriodSet, nLancero int
+	ncolsSet = 1
+	nrowsSet = 4
+	linePeriodSet = 400 // don't make this too low, or test -race will fail
+	nLancero = 3
+	source := new(LanceroSource)
+	source.devices = make(map[int]*LanceroDevice, nLancero)
+	cardDelay := []int{0} // a single card delay value works for multiple cards
+	activeCards := make([]int, nLancero)
+	for i := 0; i < nLancero; i++ {
+		lan, err := lancero.NewNoHardware(ncolsSet, nrowsSet, linePeriodSet)
+		if err != nil {
+			t.Error(err)
+		}
+		dev := LanceroDevice{card: lan}
+		dev.devnum = i
+		source.devices[i] = &dev
+		activeCards[i] = i
+		source.ncards++
+	}
+	// above is essentially NewLanceroSource
+
+	config := LanceroSourceConfig{ClockMhz: 125, CardDelay: cardDelay,
+		ActiveCards: make([]int, nLancero)}
+	if err := source.Configure(&config); err == nil && nLancero > 1 {
+		t.Error("expected error for re-using a device")
+	}
+	config = LanceroSourceConfig{ClockMhz: 125, CardDelay: cardDelay,
+		ActiveCards: activeCards}
+	if err := source.Configure(&config); err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < nLancero; i++ {
+		if config.ActiveCards[i] != config.AvailableCards[i] {
+			t.Errorf("AvailableCards not populated corrects. AvailableCards %v", config.AvailableCards)
+		}
+	}
+
+	if err := Start(source); err != nil {
+		source.Stop()
+		t.Fatal(err)
+	}
+	defer source.Stop()
+
+	// end Start(source)
+	if err := source.ConfigureMixFraction(0, 1.0); err == nil {
+		t.Error("expected error for mixing on even channel")
+	}
+	if err := source.ConfigureMixFraction(1, 1.0); err != nil {
+		t.Error(err)
+	}
+	time.Sleep(20 * time.Millisecond) // wait long enough for some data to be processed
+
 }
 
 func TestMix(t *testing.T) {
