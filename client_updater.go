5e145d5826fed97bfbf648b1826130c821400a9f
Joe Fowler <drjoefowler@gmail.com>
2018-07-09 16:44:10 -0600

    Add NUMBERWRITTEN to the unsaved messages list

--- client_updater.go
+++ client_updater.go
@@ -103,9 +103,10 @@ func RunClientUpdater(portstatus int) {
 // nosaveMessages is a set of message names that you don't save, because they
 // contain no configuration that makes sense to preserve across runs of dastard.
 var nosaveMessages = map[string]struct{}{
-	"channelnames": {},
-	"alive":        {},
-	"triggerrate":  {},
+	"channelnames":  {},
+	"alive":         {},
+	"triggerrate":   {},
+	"numberwritten": {},
 }
 
 // saveState stores server configuration to the standard config file.
