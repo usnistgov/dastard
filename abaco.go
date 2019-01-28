9efbd13c04daa90c8f5685cc2300f0f565c10bca
Joe Fowler <drjoefowler@gmail.com>
2019-01-28 10:46:12 -0700

    User-space read buffer in AbacoDevice
    Also, add placeholders and comments for activities that have to happen in Sample(), StartRun(), stop(), and launchAbacoReader().

--- abaco.go
+++ abaco.go
@@ -5,12 +5,15 @@ import (
 	"log"
 	"os"
 	"sort"
+	"time"
 )
 
 // AbacoDevice represents a single Abaco device-special file.
 type AbacoDevice struct {
-	devnum int
-	File   *os.File
+	devnum     int
+	File       *os.File
+	buffersize int
+	buffer     []byte
 }
 
 // NewAbacoDevice creates a new AbacoDevice and opens the underlying file for reading
@@ -21,14 +24,17 @@ func NewAbacoDevice(devnum int) (dev *AbacoDevice, err error) {
 	if dev.File, err = os.OpenFile(fname, os.O_RDWR, 0666); err != nil {
 		return nil, err
 	}
+	dev.buffersize = 1024 * 1024 // How to choose the size of the read buffer???
+	dev.buffer = make([]byte, dev.buffersize)
 	return dev, nil
 }
 
 // AbacoSource represents all Abaco devices that can potentially supply data.
 type AbacoSource struct {
-	devices map[int]*AbacoDevice
-	ncards  int
-	active  []*AbacoDevice
+	devices    map[int]*AbacoDevice
+	ncards     int
+	active     []*AbacoDevice
+	readPeriod time.Duration
 	AnySource
 }
 
@@ -49,7 +55,6 @@ func NewAbacoSource() (*AbacoSource, error) {
 			log.Printf("warning: failed to open /dev/xdma0_c2h_%d", dnum)
 			continue
 		}
-		// ad.card = lan
 		source.devices[dnum] = ad
 		source.ncards++
 	}
@@ -90,6 +95,7 @@ func (as *AbacoSource) Configure(config *AbacoSourceConfig) (err error) {
 		return false
 	}
 
+	// Activate the cards listed in the config request.
 	as.active = make([]*AbacoDevice, 0)
 	for i, c := range config.ActiveCards {
 		dev := as.devices[c]
@@ -112,19 +118,45 @@ func (as *AbacoSource) Configure(config *AbacoSourceConfig) (err error) {
 }
 
 // Sample determines key data facts by sampling some initial data.
+// For Abaco µMUX systems, we need to discard one DMA buffer worth of stale data,
+// then check the data after that for frame bit info.
 func (as *AbacoSource) Sample() error {
+	// Consume 1 full DMA buffer
+	// Grab some (??) data and analyze for frame bits
+	// Store that info.
 	return nil
 }
 
 // StartRun tells the hardware to switch into data streaming mode.
 // For Abaco µMUX systems, we need to consume any initial data that constitutes
-// a fraction of a frame.
+// a fraction of a frame. Then launch a goroutine to consume data.
 func (as *AbacoSource) StartRun() error {
+	// No need to switch into data streaming mode on Abaco?
+	// Consume data until start of a frame is found.
+	as.launchAbacoReader()
 	return nil
+}
+
+func (as *AbacoSource) launchAbacoReader() {
+	as.readPeriod = 50 * time.Millisecond
+	go func() {
+		ticker := time.NewTicker(as.readPeriod)
+		for {
+			select {
+			case <-as.abortSelf:
+				// close anything that needs closing
+				return
+
+			case <-ticker.C:
+				//
+			}
+		}
+	}()
 }
 
 // stop ends the data streaming on all active lancero devices.
 func (as *AbacoSource) stop() error {
+	// loop over as.active and do any needed stopping functions.
 	return nil
 }
 
