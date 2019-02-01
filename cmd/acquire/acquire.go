00cbaa62fafb5531758e9d97606b7b109707e29a
Joe Fowler <drjoefowler@gmail.com>
2019-02-01 13:03:09 -0700

    Add use of bufio, which doesn't do what I hoped.
    It allows you to make lots of Go-level reads with a single system call.
    What I wanted, instead, was to make several system calls in one Go-level call.

--- cmd/acquire/acquire.go
+++ cmd/acquire/acquire.go
@@ -3,6 +3,7 @@ package main
 import (
 	// "bytes"
 	// "encoding/binary"
+	"bufio"
 	"flag"
 	"fmt"
 	"io"
@@ -20,6 +21,7 @@ type acquireOptions struct {
 	nSamples  int
 	devnum    int
 	output    string
+	useBuffer bool
 }
 
 var opt acquireOptions
@@ -29,6 +31,7 @@ func parseOptions() error {
 	flag.IntVar(&opt.threshold, "t", 1024, "threshold (in frames), fill level interrupt")
 	flag.IntVar(&opt.nSamples, "n", 0, "number of samples to acquire (<=0 means run indenfinitely)")
 	flag.IntVar(&opt.devnum, "d", 0, "device number for /dev/xdma0_c2h_*")
+	flag.BoolVar(&opt.useBuffer, "b", false, "use buffered I/O")
 	flag.StringVar(&opt.output, "o", "", "output filename")
 	flag.Parse()
 
@@ -60,34 +63,64 @@ func acquire(abaco *dastard.AbacoDevice) (bytesRead int, err error) {
 
 	var buffer []byte
 	var nbytes int
+	bufsize := 1024*1024 - 16384*2
 
 	// Trap interrupts so we can cleanly exit the program
 	interruptCatcher := make(chan os.Signal, 1)
 	signal.Notify(interruptCatcher, os.Interrupt)
+
+	var bufReader *bufio.Reader
+	if opt.useBuffer {
+		bufReader = bufio.NewReaderSize(abaco.File, bufsize)
+	}
 
 	for {
 		select {
 		case <-interruptCatcher:
 			return
 		default:
-			bufsize := 1024 * 1024
 			buffer = make([]byte, bufsize)
-			for bytesConsumed := 0; bytesConsumed < bufsize; {
-				nbytes, err = abaco.File.Read(buffer[bytesConsumed:])
-				bytesConsumed += nbytes
-				time.Sleep(1 * time.Millisecond)
+			nreads := 0
+			if opt.useBuffer {
+				for bytesConsumed := 0; bytesConsumed < bufsize; {
+					// log.Printf("There are %d bytes ready to read", bufReader.Buffered())
+					nbytes, err = bufReader.Read(buffer)
+					bytesConsumed += nbytes
+					nreads++
+					time.Sleep(1 * time.Millisecond)
 
+					if err == io.EOF {
+						return
+					} else if err != nil {
+						log.Printf("ERROR ", err)
+						return
+					}
+				}
 				if err == io.EOF {
 					return
 				} else if err != nil {
 					log.Printf("ERROR ", err)
 					return
 				}
+			} else {
+				for bytesConsumed := 0; bytesConsumed < bufsize; {
+					nbytes, err = abaco.File.Read(buffer[bytesConsumed:])
+					bytesConsumed += nbytes
+					nreads++
+					time.Sleep(1 * time.Millisecond)
+
+					if err == io.EOF {
+						return
+					} else if err != nil {
+						log.Printf("ERROR ", err)
+						return
+					}
+				}
 			}
 
 			log.Printf("%x %x %x %x\n", buffer[0:4], buffer[4:8], buffer[8:12], buffer[12:16])
 			totalBytes := len(buffer)
-			log.Printf("Filled a buffer of full size %d", len(buffer))
+			log.Printf("Filled a buffer of full size %d in %2d reads", len(buffer), nreads)
 			log.Println()
 
 			if saveData {
