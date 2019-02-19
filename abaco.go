c421ee51db36b2c5d1d3bc67ab177df807cb3555
Joe Fowler <drjoefowler@gmail.com>
2019-02-19 15:51:04 -0700

    Add some parsing of Abaco data.

--- abaco.go
+++ abaco.go
@@ -3,6 +3,7 @@ package dastard
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"time"
@@ -16,6 +17,7 @@ type AbacoDevice struct {
 	devnum     int
 	nrows      int
 	ncols      int
+	nchan      int
 	frameSize  int // frame size, in bytes
 	File       *os.File
 	buffersize int
@@ -45,6 +47,15 @@ func NewAbacoDevice(devnum int) (dev *AbacoDevice, err error) {
 // abacoFBOffset gives the location of the frame bit is in bytes 0, 4, 8...
 const abacoFBOffset int = 0
 
+// sampleCard samples the data from a single card to scan for frame bits.
+// For Abaco µMUX systems, we need to discard one DMA buffer worth of stale data,
+// then check the data after that for frame bit info.
+// 1. Open the user file and query it for the data rate, number of channels (?),
+//    firmware FIFO size, and firmware version.
+// 2. Read at least 2 FIFOs plus enough data to discern frame bits
+// 3. Discard 2 FIFOs
+// 4. Scan for frame bits
+// 5. Store info that we've learned in AbacoSource or per device.
 func (device *AbacoDevice) sampleCard() error {
 	// Read the device: two full buffers AND ignore the leading 2 FIFOs in the 2nd
 	for i := 0; i < 2; i++ {
@@ -59,6 +70,7 @@ func (device *AbacoDevice) sampleCard() error {
 	if err3 == nil {
 		device.ncols = n
 		device.nrows = (p - q) / n
+		device.nchan = device.ncols * device.nrows
 		device.frameSize = device.ncols * device.nrows * 4
 	} else {
 		fmt.Printf("Error in FindFrameBits: %v", err3)
@@ -68,10 +80,11 @@ func (device *AbacoDevice) sampleCard() error {
 
 // AbacoSource represents all Abaco devices that can potentially supply data.
 type AbacoSource struct {
-	devices    map[int]*AbacoDevice
-	Ndevices   int
-	active     []*AbacoDevice
-	readPeriod time.Duration
+	devices     map[int]*AbacoDevice
+	Ndevices    int
+	active      []*AbacoDevice
+	readPeriod  time.Duration
+	buffersChan chan AbacoBuffersType
 	AnySource
 }
 
@@ -163,19 +176,7 @@ func (as *AbacoSource) Configure(config *AbacoSourceConfig) (err error) {
 }
 
 // Sample determines key data facts by sampling some initial data.
-// For Abaco µMUX systems, we need to discard one DMA buffer worth of stale data,
-// then check the data after that for frame bit info.
 func (as *AbacoSource) Sample() error {
-	// 1. Open the user file and query it for the data rate, number of channels (?),
-	// firmware FIFO size, and firmware version.
-	// 2. Read at least 2 FIFOs plus enough data to discern frame bits
-	// 3. Discard 2 FIFOs
-	// 4. Scan for frame bits
-	// 5. Store info that we've learned in AbacoSource or per device.
-	// 6. (not sure we'll do this) Read exactly as many bytes as needed to fill
-	// out an incomplete data frame.
-
-	// For now, assume only one active device.
 	if len(as.active) <= 0 {
 		return fmt.Errorf("No Abaco devices are active")
 	}
@@ -195,21 +196,68 @@ func (as *AbacoSource) Sample() error {
 // a fraction of a frame. Then launch a goroutine to consume data.
 func (as *AbacoSource) StartRun() error {
 	// There's no data streaming mode on Abaco, so no need to start it?
-	// Consume data until start of a frame is found.
+	// TODO: Consume data until start of a frame is found.
 	as.launchAbacoReader()
 	return nil
+}
+
+// AbacoBuffersType is an internal message type used to allow
+// a goroutine to read from the Abaco card and put data on a buffered channel
+type AbacoBuffersType struct {
+	datacopies [][]RawType
+	// lastSampleTime time.Time
+	// timeDiff       time.Duration
+	totalBytes int
 }
 
 //
 func (as *AbacoSource) launchAbacoReader() {
 	timeoutPeriod := 2 * time.Second
 	go func() {
+		as.buffersChan = make(chan AbacoBuffersType, 100)
+		defer close(as.buffersChan)
 		timeout := time.NewTicker(timeoutPeriod)
-		// Create a channel for data to return on
+
 		for {
 			go func() {
-				// read from /dev/xdma0_c2h_0
+				// read from the data pipe
 				// send filled buffer (and bytes actually read) on a channel
+				framesUsed := math.MaxInt64
+				totalBytes := 0
+				datacopies := make([][]RawType, as.nchan)
+				nchanPrevDevices := 0
+				for _, dev := range as.active {
+					nb, err := dev.File.Read(dev.buffer)
+					if err != nil {
+						panic("AbacoDevice.File.Read failed")
+					}
+					bframes := nb / dev.frameSize
+					if bframes < framesUsed {
+						framesUsed = bframes
+					}
+
+					localBuffer := make([]byte, nb)
+					copy(localBuffer, dev.buffer[:nb])
+					rawBuffer := bytesToRawType(localBuffer)
+					for i := 0; i < dev.nchan; i++ {
+						datacopies[i+nchanPrevDevices] = make([]RawType, framesUsed)
+						dc := datacopies[i+nchanPrevDevices]
+						idx := i
+						for j := 0; j < framesUsed; j++ {
+							dc[j] = rawBuffer[idx]
+							idx += dev.nchan
+						}
+					}
+					totalBytes += nb
+				}
+				if len(as.buffersChan) == cap(as.buffersChan) {
+					panic(fmt.Sprintf("internal buffersChan full, len %v, capacity %v", len(as.buffersChan), cap(as.buffersChan)))
+				}
+				as.buffersChan <- AbacoBuffersType{
+					datacopies: datacopies,
+					// lastSampleTime: lastSampleTime,
+					// timeDiff: timeDiff,
+					totalBytes: totalBytes}
 			}()
 			select {
 			case <-as.abortSelf:
@@ -218,15 +266,17 @@ func (as *AbacoSource) launchAbacoReader() {
 
 			case <-timeout.C:
 				// Handle failure to return
+				log.Printf("Abaco read timed out")
+				return
 
-				// case <-data channel:
+			case <-as.buffersChan:
 				// Send the data on for further processing
 			}
 		}
 	}()
 }
 
-// stop ends the data streaming on all active lancero devices.
+// stop ends the data streaming on all active abaco devices.
 func (as *AbacoSource) stop() error {
 	// loop over as.active and do any needed stopping functions.
 	return nil
