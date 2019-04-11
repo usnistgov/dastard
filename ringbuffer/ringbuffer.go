1b9e4cbd3a23119be0dc6c903f705f07a5f9b97b
Joe Fowler <drjoefowler@gmail.com>
2019-04-10 22:22:47 -0600

    Add RingBuffer.create to test new buffers

--- ringbuffer/ringbuffer.go
+++ ringbuffer/ringbuffer.go
@@ -1,6 +1,7 @@
 package ringbuffer
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 
@@ -16,12 +17,13 @@ type bufferDescription struct {
 
 // RingBuffer describes the shared-memory ring buffer filled by DEED.
 type RingBuffer struct {
-	desc     []byte
-	raw      []byte
-	rawName  string
-	descName string
-	rawFile  *os.File
-	descFile *os.File
+	desc      []byte
+	raw       []byte
+	rawName   string
+	descName  string
+	rawFile   *os.File
+	descFile  *os.File
+	writeable bool // Is this a writeable buffer? False, except for testing
 }
 
 // NewRingBuffer creates and returns a new RingBuffer object
@@ -30,6 +32,52 @@ func NewRingBuffer(rawName, descName string) (rb *RingBuffer, err error) {
 	rb.rawName = rawName
 	rb.descName = descName
 	return rb, nil
+}
+
+// create make a writeable buffer and is only for testing
+func (rb *RingBuffer) create(bufsize int) (err error) {
+	rb.writeable = true
+	file, err := shm.Open(rb.descName, os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		return err
+	}
+	rb.descFile = file
+	fd := int(rb.descFile.Fd())
+	size := 4096
+	if err = syscall.Ftruncate(fd, int64(size)); err != nil {
+		fmt.Printf("Problem is here")
+		return err
+	}
+	rb.desc, err = syscall.Mmap(fd, 0, size, syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if err != nil {
+		return err
+	}
+
+	file, err = shm.Open(rb.rawName, os.O_RDWR|os.O_CREATE, 0660)
+	if err != nil {
+		return err
+	}
+	rb.rawFile = file
+	fd = int(rb.rawFile.Fd())
+	if err = syscall.Ftruncate(fd, int64(bufsize)); err != nil {
+		return err
+	}
+	rb.raw, err = syscall.Mmap(fd, 0, bufsize, syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// unlink removes a writeable buffer and is only for testing
+func (rb *RingBuffer) unlink() (err error) {
+	if err = shm.Unlink(rb.rawName); err != nil {
+		return err
+	}
+	if err = shm.Unlink(rb.descName); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Open opens the ring buffer shared memory regions and memory maps them.
--- ringbuffer/ringbuffer_test.go
+++ ringbuffer/ringbuffer_test.go
@@ -5,10 +5,22 @@ import (
 )
 
 func TestBufferOpenClose(t *testing.T) {
-	goodname1 := "abaco_ring_buffer"
-	goodname2 := "abaco_ring_description"
+	goodname1 := "will_exist_buffer"
+	goodname2 := "will_exist_description"
 	badname1 := "does_not_exist"
 	badname2 := "does_not_exist_either"
+
+	// Now test the writeable buffer, which we need for testing.
+	writebuf, err := NewRingBuffer(goodname1, goodname2)
+	if err != nil {
+		t.Error("Failed NewRingBuffer")
+	}
+	defer writebuf.unlink()
+	if err = writebuf.create(8192); err != nil {
+		t.Error("Failed RingBuffer.create", err)
+	}
+
+	// Now 2 buffers that should not be Openable
 	r, err := NewRingBuffer(badname1, badname2)
 	if err != nil {
 		t.Error("Failed NewRingBuffer", err)
@@ -24,16 +36,49 @@ func TestBufferOpenClose(t *testing.T) {
 		t.Errorf("NewRingBuffer(%s, %s) succeeds, should fail", badname1, goodname2)
 	}
 
+	// This buffer should be Openable and Closeable
 	r, err = NewRingBuffer(goodname1, goodname2)
 	if err != nil {
 		t.Error("Failed NewRingBuffer", err)
 	}
-
 	if err = r.Open(); err != nil {
 		t.Error("Failed RingBuffer.Open", err)
 	}
-
 	if err = r.Close(); err != nil {
 		t.Error("Failed RingBuffer.Close", err)
+	}
+
+	if err = writebuf.Close(); err != nil {
+		t.Error("Failed RingBuffer.Close", err)
+	}
+}
+
+func TestBufferWriteRead(t *testing.T) {
+	name1 := "test_ring_buffer"
+	name2 := "test_ring_description"
+	writebuf, err := NewRingBuffer(name1, name2)
+	if err != nil {
+		t.Error("Failed NewRingBuffer", err)
+	}
+	if err = writebuf.create(8192); err != nil {
+		t.Error("Failed RingBuffer.create", err)
+	}
+
+	b, err := NewRingBuffer(name1, name2)
+	if err != nil {
+		t.Error("Failed NewRingBuffer", err)
+	}
+	if err = b.Open(); err != nil {
+		t.Error("Failed RingBuffer.Open", err)
+	}
+
+	if err = b.Close(); err != nil {
+		t.Error("Failed RingBuffer.Close", err)
+	}
+	if err = writebuf.Close(); err != nil {
+		t.Error("Failed RingBuffer.Close", err)
+	}
+	if err = writebuf.unlink(); err != nil {
+		t.Error("Failed RingBuffer.unlink", err)
 	}
 }
