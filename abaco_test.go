1f5ff7ff567dcba743267ed7e06e67aadb6bef88
Joseph Fowler <drjoefowler@gmail.com>
2019-09-26 15:24:21 -0600

    Generate Abaco fake packets. Put in and read from RingBuffer

--- abaco_test.go
+++ abaco_test.go
@@ -1,13 +1,26 @@
 package dastard
 
 import (
+	"fmt"
 	"math"
 	"testing"
 
 	"github.com/usnistgov/dastard/packets"
+	"github.com/usnistgov/dastard/ringbuffer"
 )
 
 func TestGeneratePackets(t *testing.T) {
+
+	ringname := "ring_test_buffer"
+	ringdesc := "ring_test_description"
+	rb, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
+	if err != nil {
+		t.Fatalf("Could not open ringbuffer: %s", err)
+	}
+	defer rb.Unlink()
+	if err = rb.Create(128 * 8192); err != nil {
+		t.Fatalf("Failed RingBuffer.Create: %s", err)
+	}
 
 	p := packets.NewPacket(10, 20, 30, 0)
 
@@ -15,7 +28,7 @@ func TestGeneratePackets(t *testing.T) {
 	const Nsamp = 20000
 	d := make([]int16, Nchan*Nsamp)
 	for i := 0; i < Nchan; i++ {
-		freq := (float64(i) + 2.0) / float64(Nsamp)
+		freq := (float64(i + 2)) / float64(Nsamp)
 		for j := 0; j < Nsamp; j++ {
 			d[i+Nchan*j] = int16(30000.0 * math.Cos(freq*float64(j)))
 		}
@@ -25,10 +38,20 @@ func TestGeneratePackets(t *testing.T) {
 	if stride*Nchan*2 > 8000 {
 		t.Fatalf("Packet payload size %d exceeds 8000 bytes", stride*Nchan*2)
 	}
+	empty := make([]byte, 8192)
 	dims := []int16{Nchan}
 	for repeats := 0; repeats < 3; repeats++ {
 		for i := 0; i < Nsamp; i += stride {
-			p.NewData(d[i:i+stride], dims)
+			p.NewData(d[i:i+stride*Nchan], dims)
+			b := p.Bytes()
+			b = append(b, empty[:8192-len(b)]...)
+			rb.Write(b)
 		}
+		contents, err := rb.ReadMultipleOf(8192)
+		if err != nil {
+			t.Errorf("Could not read buffer: %s", err)
+		}
+
+		fmt.Printf("Size read, still in buffer: %d %d\n", len(contents), rb.BytesReadable())
 	}
 }
