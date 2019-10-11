4f529f8d25d983afd601f14e531b49f9a1012b0b
Joe Fowler <drjoefowler@gmail.com>
2019-10-11 00:19:19 -0600

    A helpful script to generate fake Abaco packets
    Generates 20k samples (*8 channels) every 40 ms.
    
    Gives problems with dc.py, though...

---
+++ cmd/abaco/generate_abaco.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+
+	"github.com/usnistgov/dastard/packets"
+	"github.com/usnistgov/dastard/ringbuffer"
+)
+
+func main() {
+	cardnum := 3
+
+	ringname := fmt.Sprintf("xdma%d_c2h_0_buffer", cardnum)
+	ringdesc := fmt.Sprintf("xdma%d_c2h_0_description", cardnum)
+	ring, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
+	if err != nil {
+		msg := fmt.Sprintf("Could not open ringbuffer: %s", err)
+		panic(msg)
+	}
+	ring.Unlink()       // in case it exists from before
+	defer ring.Unlink() // so it won't exist after
+	const packetAlign = 8192
+	if err = ring.Create(256 * packetAlign); err != nil {
+		msg := fmt.Sprintf("Failed RingBuffer.Create: %s", err)
+		panic(msg)
+	}
+
+	const Nchan = 8
+	const Nsamp = 20000
+	const stride = 500 // We'll put this many samples into a packet
+	if stride*Nchan*2 > 8000 {
+		msg := fmt.Sprintf("Packet payload size %d exceeds 8000 bytes", stride*Nchan*2)
+		panic(msg)
+	}
+
+	fmt.Printf("Generating data in shm:%s\n", ringname)
+	p := packets.NewPacket(10, 20, 0x100, 0)
+	d := make([]int16, Nchan*Nsamp)
+	for i := 0; i < Nchan; i++ {
+		freq := (float64(i + 2)) / float64(Nsamp)
+		for j := 0; j < Nsamp; j++ {
+			d[i+Nchan*j] = int16(30000.0 * math.Cos(freq*float64(j)))
+		}
+	}
+
+	empty := make([]byte, packetAlign)
+	dims := []int16{Nchan}
+	timer := time.NewTicker(40 * time.Millisecond)
+	for {
+		select {
+		case <-timer.C:
+			for i := 0; i < Nsamp; i += stride {
+				p.NewData(d[i:i+stride*Nchan], dims)
+				b := p.Bytes()
+				b = append(b, empty[:packetAlign-len(b)]...)
+				if ring.BytesWriteable() >= len(b) {
+					ring.Write(b)
+				}
+			}
+		}
+	}
+}
