usnistgov/dastard

commit 94c373f3339a178d601755110766a1fd4ab24bc1

handle data drops for lancero source
When the first frame bit is in the incorrect location, we note a data drop. Then we estimate how many frames were dropped based on cpu time and sample rate. Then we write to a file ending in "data_drop.txt" with the framenum and estimated number of frames dropped. We also send a DATADROP message and flush the data drop file every 10 seconds. I modeled the code on how external triggers are handled to the extenet possible.

data_source.go
@@ -165,7 +165,7 @@ func CoreLoop(ds DataSource, queuedRequests chan func()) {
 			}
 			if err := ds.ProcessSegments(block); err != nil {
 				log.Printf("AnySource.ProcessSegments returns Error; stopping source: %s\n", err.Error())
-				panic("panic stops source when processSegments fails")
+				panic("panic to stop source when processSegments, this seems to keep the lancero working better than stopping the source")
 			}
 			// In some sources, ds.getNextBlock has to be called again to initiate the next
 			// data acquisition step (Lancero specifically).
@@ -244,21 +244,22 @@ type dataBlock struct {
 // AnySource implements features common to any object that implements
 // DataSource, including the output channels and the abort channel.
 type AnySource struct {
-	nchan        int           // how many channels to provide
-	name         string        // what kind of source is this?
-	chanNames    []string      // one name per channel
-	chanNumbers  []int         // names have format "prefixNumber", this is the number
-	rowColCodes  []RowColCode  // one RowColCode per channel
-	voltsPerArb  []float32     // the physical units per arb, one per channel
-	sampleRate   float64       // samples per second
-	samplePeriod time.Duration // time per sample
-	lastread     time.Time
-	nextFrameNum FrameIndex // frame number for the next frame we will receive
-	processors   []*DataStreamProcessor
-	abortSelf    chan struct{}   // Signal to the core loop of active sources to stop
-	nextBlock    chan *dataBlock // Signal from the core loop that a block is ready to process
-	broker       *TriggerBroker
-	configError  error // Any error that arose when configuring the source (before Start)
+	nchan                  int           // how many channels to provide
+	name                   string        // what kind of source is this?
+	chanNames              []string      // one name per channel
+	chanNumbers            []int         // names have format "prefixNumber", this is the number
+	rowColCodes            []RowColCode  // one RowColCode per channel
+	voltsPerArb            []float32     // the physical units per arb, one per channel
+	sampleRate             float64       // samples per second
+	samplePeriod           time.Duration // time per sample
+	lastread               time.Time
+	nextFrameNum           FrameIndex // frame number for the next frame we will receive
+	previousLastSampleTime time.Time
+	processors             []*DataStreamProcessor
+	abortSelf              chan struct{}   // Signal to the core loop of active sources to stop
+	nextBlock              chan *dataBlock // Signal from the core loop that a block is ready to process
+	broker                 *TriggerBroker
+	configError            error // Any error that arose when configuring the source (before Start)
 
 	shouldAutoRestart   bool // used to tell SourceControl to try to restart this source after an error
 	noProcess           bool // Set true only for testing.
@@ -291,11 +292,11 @@ func (ds *AnySource) getPulseLengths() (int, int, error) {
 // It's a more synchronous version of each dsp launching its own goroutine
 func (ds *AnySource) ProcessSegments(block *dataBlock) error {
 	var wg sync.WaitGroup
+	if len(ds.processors) != len(block.segments) {
+		panic(fmt.Sprintf("Oh crap! block has %d segments but Source has %d processors",
+			len(block.segments), len(ds.processors)))
+	}
 	for i, dsp := range ds.processors {
-		if i >= len(block.segments) {
-			fmt.Printf("Oh crap! block has %d segments but Source has %d processors",
-				len(block.segments), len(ds.processors))
-		}
 		segment := block.segments[i]
 		wg.Add(1)
 		go func(dsp *DataStreamProcessor) {
@@ -319,8 +320,12 @@ func (ds *AnySource) ProcessSegments(block *dataBlock) error {
 	for i, dsp := range ds.processors {
 		numberWritten[i] = dsp.numberWritten
 	}
-	err := ds.HandleExternalTriggers(block.externalTriggerRowcounts)
-	if err != nil {
+	if err := ds.HandleExternalTriggers(block.externalTriggerRowcounts); err != nil {
+		return err
+	}
+
+	// all segments will have the same value for droppedFrames and firstFramenum, so we just look at the first segment here
+	if err := ds.HandlePotentialDroppedData(block.segments[0].droppedFrames, int(block.segments[0].firstFramenum)); err != nil {
 		return err
 	}
 	if ds.writingState.Active && !ds.writingState.Paused {
@@ -338,6 +343,52 @@ func (ds *AnySource) ProcessSegments(block *dataBlock) error {
 // the file is created upon the first call to this function for a given file writing
 func (ds *AnySource) SetExperimentStateLabel(timestamp time.Time, stateLabel string) error {
 	return ds.writingState.SetExperimentStateLabel(timestamp, stateLabel)
+}
+
+//HandlePotentialDroppedData writes to a file in the case that a data drop is detected
+//data drop refers to a case where a read from a source, eg the LanceroSource misses some frames of data
+func (ds *AnySource) HandlePotentialDroppedData(droppedFrames, firstFramenum int) error {
+
+	if ds.writingState.dataDropFileBufferedWriter == nil && droppedFrames > 0 {
+		// create file
+		var err error
+		ds.writingState.dataDropFile, err = os.Create(ds.writingState.DataDropFilename)
+		if err != nil {
+			return fmt.Errorf("cannot create DataDropFile, %v", err)
+		}
+		ds.writingState.dataDropFileBufferedWriter = bufio.NewWriter(ds.writingState.dataDropFile)
+		// write header
+		_, err = ds.writingState.dataDropFileBufferedWriter.WriteString("# firstFramenum after drop, number of dropped frames\n")
+		if err != nil {
+			return fmt.Errorf("cannot write header to dataDroFileBufferedWriter, err %v", err)
+		}
+	}
+	ds.writingState.dataDropsObserved += 1
+	if droppedFrames > 0 {
+		line := fmt.Sprintf("%v,%v", firstFramenum, droppedFrames)
+		_, err := ds.writingState.dataDropFileBufferedWriter.WriteString(line)
+		if err != nil {
+			return fmt.Errorf("cannot write to externalTriggerFileBufferedWriter, err %v", err)
+		}
+		fmt.Printf("DATA DROP. firstFramenum %v, droppedFrames %v\n", firstFramenum, droppedFrames)
+	}
+	select { // occasionally flush file and send messae about number of observed data drops
+	case <-ds.writingState.dataDropTicker.C:
+		// flush file
+		if ds.writingState.dataDropFileBufferedWriter != nil {
+			err := ds.writingState.dataDropFileBufferedWriter.Flush()
+			if err != nil {
+				return fmt.Errorf("cannot flush dataDropFileBufferedWriter, err %v", err)
+			}
+		}
+		// send message
+		clientMessageChan <- ClientUpdate{tag: "DATADROP",
+			state: struct {
+				TotalObserved int
+			}{TotalObserved: ds.writingState.dataDropsObserved}} // only exported fields are serialized
+	default:
+	}
+	return nil
 }
 
 //HandleExternalTriggers writes external trigger to a file, creates that file if neccesary, and sends out messages
@@ -647,6 +698,7 @@ func (ds *AnySource) PrepareRun(Npresamples int, Nsamples int) error {
 
 	ds.numberWrittenTicker = time.NewTicker(1 * time.Second)
 	ds.writingState.externalTriggerTicker = time.NewTicker(time.Second * 1)
+	ds.writingState.dataDropTicker = time.NewTicker(time.Second * 10)
 
 	// Launch goroutines to drain the data produced by this source
 	ds.processors = make([]*DataStreamProcessor, ds.nchan)
@@ -779,6 +831,7 @@ type DataSegment struct {
 	framePeriod     time.Duration
 	voltsPerArb     float32
 	processed       bool
+	droppedFrames   int // normally zero, positive if dropped frames detected
 	// facts about the data source?
 }
 

lancero_source.go
@@ -38,10 +38,11 @@ const lanceroFBOffset int = 2
 // BuffersChanType is an internal message type used to allow
 // a goroutine to read from the Lancero card and put data on a buffered channel
 type BuffersChanType struct {
-	datacopies     [][]RawType
-	lastSampleTime time.Time
-	timeDiff       time.Duration
-	totalBytes     int
+	datacopies       [][]RawType
+	lastSampleTime   time.Time
+	timeDiff         time.Duration
+	totalBytes       int
+	dataDropDetected bool
 }
 
 // LanceroSource is a DataSource that handles 1 or more lancero devices.
@@ -59,6 +60,7 @@ type LanceroSource struct {
 	mixRequests              chan *MixFractionObject
 	currentMix               chan []float64 // allows ConfigureMixFraction to return the currentMix race free
 	externalTriggerLastState bool
+	previousLastSampleTime   time.Time
 	AnySource
 }
 
@@ -569,14 +571,31 @@ func (ls *LanceroSource) launchLanceroReader() {
 				var buffers [][]RawType
 				framesUsed := math.MaxInt64
 				var lastSampleTime time.Time
-
+				var dataDropDetected bool
 				for _, dev := range ls.active {
 
 					b, timeFix, err := dev.card.AvailableBuffer()
 					if err != nil {
 						panic("Warning: AvailableBuffer failed")
 					}
-					buffers = append(buffers, bytesToRawType(b))
+					q, p, ncols, err := lancero.FindFrameBits(b, lanceroFBOffset)
+					nrows := (p - q) / ncols
+					if ncols != dev.ncols || nrows != dev.nrows {
+						panic(fmt.Sprintf("ncols have %v, want %v. nrows have %v, want %v",
+							ncols, dev.ncols, nrows, dev.nrows))
+					}
+					firstWord := q
+					if firstWord > 0 {
+						dataDropDetected = true
+						// align to next frame start
+						buffers = append(buffers, bytesToRawType(b[firstWord*4:]))
+						log.Printf("DATA DROP, first word = %v\n", firstWord)
+						if len(ls.active) > 1 {
+							panic("dropped data, handling multiple devices not yet implemented")
+						}
+					} else {
+						buffers = append(buffers, bytesToRawType(b))
+					}
 					bframes := len(b) / dev.frameSize
 					if bframes < framesUsed {
 						framesUsed = bframes
@@ -588,25 +607,7 @@ func (ls *LanceroSource) launchLanceroReader() {
 					fmt.Println("timeDiff in lancero reader", timeDiff)
 				}
 				ls.lastread = lastSampleTime
-				// check for changes in nrow, ncol and lsync
-				for ibuf, dev := range ls.active {
-					buffer := buffers[ibuf]
-					q, p, n, err := lancero.FindFrameBits(rawTypeToBytes(buffer), lanceroFBOffset)
-					if err != nil {
-						panic(fmt.Sprintf("Error in findFrameBits: %v", err))
-					}
-					qExpect := dev.ncols * dev.nrows
-					// FindFrameBits q is the index of the first frame bit after a non frame bit
-					ncols := n
-					nrows := (p - q) / n
-					periodNS := timeDiff.Nanoseconds() / int64(framesUsed)
-					lsync := roundint((float64(periodNS) / 1000) * float64(dev.clockMhz) / float64(nrows))
-					if q != qExpect || ncols != dev.ncols || nrows != dev.nrows || framesUsed <= 0 {
-						fmt.Printf("(Not checking lsync) have ibuf %v, q %v, ncols %v, nrows %v, lsync %v, framesUsed %v\nwant q %v, ncols %v, nrows %v, lsync %v, dataBlockCount %v\n",
-							ibuf, q, ncols, nrows, lsync, framesUsed, qExpect, dev.ncols, dev.nrows, dev.lsync, ls.dataBlockCount)
-						panic("error reading from lancero, probably let buffer overfill")
-					}
-				}
+
 				// Consume framesUsed frames of data from each channel.
 				// Careful! This slice of slices will be in lancero READOUT order:
 				// r0c0, r0c1, r0c2, etc.
@@ -646,7 +647,7 @@ func (ls *LanceroSource) launchLanceroReader() {
 					panic(fmt.Sprintf("internal buffersChan full, len %v, capacity %v", len(ls.buffersChan), cap(ls.buffersChan)))
 				}
 				ls.buffersChan <- BuffersChanType{datacopies: datacopies, lastSampleTime: lastSampleTime,
-					timeDiff: timeDiff, totalBytes: totalBytes}
+					timeDiff: timeDiff, totalBytes: totalBytes, dataDropDetected: dataDropDetected}
 			}
 		}
 	}()
@@ -714,6 +715,7 @@ func (ls *LanceroSource) distributeData(buffersMsg BuffersChanType) *dataBlock {
 	timeDiff := buffersMsg.timeDiff
 	totalBytes := buffersMsg.totalBytes
 	framesUsed := len(datacopies[0])
+	dataDropDetected := buffersMsg.dataDropDetected
 
 	// Backtrack to find the time associated with the first sample.
 	segDuration := time.Duration(roundint((1e9 * float64(framesUsed-1)) / ls.sampleRate))
@@ -744,6 +746,12 @@ func (ls *LanceroSource) distributeData(buffersMsg BuffersChanType) *dataBlock {
 	}
 	block.externalTriggerRowcounts = externalTriggerRowcounts
 
+	var droppedFrames int
+	if dataDropDetected {
+		droppedDuration := lastSampleTime.Sub(ls.previousLastSampleTime)
+		droppedFrames = roundint(droppedDuration.Seconds() * ls.sampleRate)
+	}
+
 	for channelIndex := 0; channelIndex < nchan; channelIndex++ {
 		data := datacopies[ls.chan2readoutOrder[channelIndex]]
 		isFeedbackChannel := (channelIndex%2 == 1)
@@ -757,14 +765,16 @@ func (ls *LanceroSource) distributeData(buffersMsg BuffersChanType) *dataBlock {
 			rawData:         data,
 			framesPerSample: 1, // This will be changed later if decimating
 			framePeriod:     ls.samplePeriod,
-			firstFramenum:   ls.nextFrameNum,
+			firstFramenum:   ls.nextFrameNum + FrameIndex(droppedFrames),
 			firstTime:       firstTime,
 			signed:          !isFeedbackChannel,
+			droppedFrames:   droppedFrames,
 		}
 		block.segments[channelIndex] = seg
 		block.nSamp = len(data)
 	}
 	ls.nextFrameNum += FrameIndex(framesUsed)
+	ls.previousLastSampleTime = lastSampleTime
 	if ls.heartbeats != nil {
 		ls.heartbeats <- Heartbeat{Running: true, DataMB: float64(totalBytes) / 1e6,
 			Time: timeDiff.Seconds()}

writing_state.go
@@ -23,6 +23,11 @@ type WritingState struct {
 	externalTriggerFileBufferedWriter *bufio.Writer
 	externalTriggerTicker             *time.Ticker
 	externalTriggerFile               *os.File
+	DataDropFilename                  string
+	dataDropsObserved                 int
+	dataDropFileBufferedWriter        *bufio.Writer
+	dataDropTicker                    *time.Ticker
+	dataDropFile                      *os.File
 	sync.Mutex
 }
 
@@ -61,6 +66,7 @@ func (ws *WritingState) Start(filenamePattern, path string) error {
 	ws.FilenamePattern = filenamePattern
 	ws.ExperimentStateFilename = fmt.Sprintf(filenamePattern, "experiment_state", "txt")
 	ws.ExternalTriggerFilename = fmt.Sprintf(filenamePattern, "external_trigger", "bin")
+	ws.DataDropFilename = fmt.Sprintf(filenamePattern, "data_drop", "txt")
 	return ws.setExperimentStateLabel(time.Now(), "START")
 }
 
@@ -94,8 +100,19 @@ func (ws *WritingState) Stop() error {
 		ws.externalTriggerFileBufferedWriter = nil
 		ws.externalTriggerFile = nil
 	}
+	if ws.dataDropFile != nil {
+		if err := ws.dataDropFileBufferedWriter.Flush(); err != nil {
+			return fmt.Errorf("failed to flush externalTriggerFileBufferedWriter, err: %v", err)
+		}
+		if err := ws.externalTriggerFile.Close(); err != nil {
+			return fmt.Errorf("failed to close externalTriggerFileWriter, err: %v", err)
+		}
+		ws.dataDropFileBufferedWriter = nil
+		ws.dataDropFile = nil
+	}
 	ws.externalTriggerNumberObserved = 0
 	ws.ExternalTriggerFilename = ""
+	ws.DataDropFilename = ""
 	return nil
 }
 

commit 0b8dea0d7bce897eda693defbac100d0a495f829

Improve the *_data_drop.txt log of data drops
The previous text was missing a "\n" and was therefore unreadable. Bug was introduced in 94c373f333 when the log was first created. Oops. Fixes #241.

data_source.go
@@ -398,6 +398,7 @@ func (ds *AnySource) HandleDataDrop(droppedFrames, firstFramenum int) error {
 		ds.writingState.dataDropsObserved++
 		fmt.Printf("DATA DROP. firstFramenum %v, droppedFrames %v\n", firstFramenum, droppedFrames)
 		if ds.writingState.IsActive() {
+			// Set up the log file if not already done
 			if ds.writingState.dataDropFileBufferedWriter == nil {
 				// create file
 				var err error
@@ -408,12 +409,14 @@ func (ds *AnySource) HandleDataDrop(droppedFrames, firstFramenum int) error {
 				}
 				ds.writingState.dataDropFileBufferedWriter = bufio.NewWriter(ds.writingState.dataDropFile)
 				// write header
-				_, err = ds.writingState.dataDropFileBufferedWriter.WriteString("# firstFramenum after drop, number of dropped frames\n")
+				_, err = ds.writingState.dataDropFileBufferedWriter.WriteString("# first framenum after drop, number of dropped frames\n")
 				if err != nil {
 					return fmt.Errorf("cannot write header to dataDroFileBufferedWriter, err %v", err)
 				}
 			}
-			line := fmt.Sprintf("%v,%v", firstFramenum, droppedFrames)
+
+			// Log to the dataDropFile
+			line := fmt.Sprintf("%12d %8d\n", firstFramenum, droppedFrames)
 			_, err := ds.writingState.dataDropFileBufferedWriter.WriteString(line)
 			if err != nil {
 				return fmt.Errorf("cannot write to externalTriggerFileBufferedWriter, err %v", err)