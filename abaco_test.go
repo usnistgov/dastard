c2373380810c4f1170a493f3d3414adea6c1cb23
Joe Fowler <drjoefowler@gmail.com>
2022-03-16 12:42:44 -0600

    Restore 2 test files accidentally deleted

---
+++ abaco_test.go
@@ -0,0 +1,376 @@
+package dastard
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/usnistgov/dastard/packets"
+	"github.com/usnistgov/dastard/ringbuffer"
+)
+
+func TestGeneratePackets(t *testing.T) {
+
+	ringname := "ring_test_buffer"
+	ringdesc := "ring_test_description"
+	rb, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
+	if err != nil {
+		t.Fatalf("Could not open ringbuffer: %s", err)
+	}
+	defer rb.Unlink()
+	const packetAlign = 8192
+	if err = rb.Create(128 * packetAlign); err != nil {
+		t.Fatalf("Failed RingBuffer.Create: %s", err)
+	}
+
+	p := packets.NewPacket(10, 20, 0x100, 0)
+
+	const Nchan = 8
+	const Nsamp = 20000
+	d := make([]int16, Nchan*Nsamp)
+	for i := 0; i < Nchan; i++ {
+		freq := (float64(i + 2)) / float64(Nsamp)
+		for j := 0; j < Nsamp; j++ {
+			d[i+Nchan*j] = int16(30000.0 * math.Cos(freq*float64(j)))
+		}
+	}
+
+	const stride = 400 // We'll put this many samples into a packet
+	if stride*Nchan*2 > 8000 {
+		t.Fatalf("Packet payload size %d exceeds 8000 bytes", stride*Nchan*2)
+	}
+	empty := make([]byte, packetAlign)
+	dims := []int16{Nchan}
+	for repeats := 0; repeats < 3; repeats++ {
+		for i := 0; i < Nsamp; i += stride {
+			p.NewData(d[i:i+stride*Nchan], dims)
+			b := p.Bytes()
+			b = append(b, empty[:packetAlign-len(b)]...)
+			rb.Write(b)
+		}
+		// Consume packetSize
+		contents, err := rb.ReadMultipleOf(packetAlign)
+		if err != nil {
+			t.Errorf("Could not read buffer: %s", err)
+		}
+		r := bytes.NewReader(contents)
+		for {
+			bytesRemaining := r.Len()
+			_, err := packets.ReadPacket(r)
+			bytesUsed := bytesRemaining - r.Len()
+			if bytesUsed%packetAlign > 0 {
+				if _, err = r.Seek(int64(packetAlign-(bytesUsed%packetAlign)), io.SeekCurrent); err != nil {
+					t.Errorf("Could not seek")
+				}
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				t.Errorf("Error reading packets: %s", err)
+				break
+			}
+		}
+	}
+}
+
+func TestAbacoRing(t *testing.T) {
+	if _, err := NewAbacoRing(99999); err == nil {
+		t.Errorf("NewAbacoRing(99999) succeeded, want failure")
+	}
+	rand.Seed(time.Now().UnixNano())
+	cardnum := -rand.Intn(99998) - 1 // Rand # between -1 and -99999
+	dev, err := NewAbacoRing(cardnum)
+	if err != nil {
+		t.Fatalf("NewAbacoRing(%d) fails: %s", cardnum, err)
+	}
+
+	ringname := fmt.Sprintf("xdma%d_c2h_0_buffer", cardnum)
+	ringdesc := fmt.Sprintf("xdma%d_c2h_0_description", cardnum)
+	ring, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
+	if err != nil {
+		t.Fatalf("Could not open ringbuffer: %s", err)
+	}
+	defer ring.Unlink()
+	const packetAlign = 8192
+	if err = ring.Create(128 * packetAlign); err != nil {
+		t.Fatalf("Failed RingBuffer.Create: %s", err)
+	}
+	timeout := 2000 * time.Millisecond
+	dev.start()
+	dev.samplePackets(timeout)
+
+	p := packets.NewPacket(10, 20, 0x100, 0)
+	const Nchan = 8
+	const Nsamp = 20000
+	d := make([]int16, Nchan*Nsamp)
+	for i := 0; i < Nchan; i++ {
+		freq := (float64(i + 2)) / float64(Nsamp)
+		for j := 0; j < Nsamp; j++ {
+			d[i+Nchan*j] = int16(30000.0 * math.Cos(freq*float64(j)))
+		}
+	}
+
+	const stride = 500 // We'll put this many samples into a packet
+	if stride*Nchan*2 > 8000 {
+		t.Fatalf("Packet payload size %d exceeds 8000 bytes", stride*Nchan*2)
+	}
+	empty := make([]byte, packetAlign)
+	dims := []int16{Nchan}
+	const counterRate = 1e9
+	counter := uint64(0)
+	for repeats := 0; repeats < 6; repeats++ {
+		// Repeat the data buffer `d` once per outer loop iteration
+		for i := 0; i < Nsamp; i += stride {
+			// Each inner loop iteration generates 1 packet. Nsamp/stride packets completes buffer `d`.
+			p.NewData(d[i:i+stride*Nchan], dims)
+			ts := packets.MakeTimestamp(uint16(counter>>32), uint32(counter), counterRate)
+			p.SetTimestamp(ts)
+			b := p.Bytes()
+
+			b = append(b, empty[:packetAlign-len(b)]...)
+			if ring.BytesWriteable() >= len(b) {
+				ring.Write(b)
+			}
+			counter += stride * 1000
+		}
+		time.Sleep(Nsamp * time.Microsecond) // sample rate is 1/μs.
+	}
+	ps, err := dev.ReadAllPackets()
+	if err != nil {
+		t.Errorf("Error on AbacoRing.ReadAllPackets: %v", err)
+	}
+	if len(ps) <= 0 {
+		t.Errorf("AbacoRing.samplePackets returned only %d packets", len(ps))
+	}
+	const enable = true
+	const resetAfter = 20000
+	const bias = false
+	const pulseSign = +1
+	group := NewAbacoGroup(gIndex(ps[0]), enable, bias, resetAfter, pulseSign)
+	group.queue = append(group.queue, ps...)
+	err = group.samplePackets()
+	if err != nil {
+		t.Errorf("Error on AbacoGroup.samplePackets: %v", err)
+	}
+
+	if group.nchan != Nchan {
+		t.Errorf("group.nchan=%d, want %d", group.nchan, Nchan)
+	}
+	if group.sampleRate <= 0.0 {
+		t.Errorf("group.sampleRate=%.4g, want >0", group.sampleRate)
+	}
+}
+
+func TestAbacoUDP(t *testing.T) {
+	if _, err := NewAbacoUDPReceiver("nonexistenthost.remote.internet:4999"); err == nil {
+		t.Errorf("NewAbacoUDPReceiver(\"nonexistenthost.remote.internet:4999\") succeeded, want failure")
+	}
+	device, err := NewAbacoUDPReceiver("localhost:4999")
+	if err != nil {
+		t.Errorf("NewAbacoUDPReceiver(\"localhost:4999\") failed: %v", err)
+	}
+	err = device.start()
+	if err != nil {
+		t.Errorf("AbacoUDP.start() failed: %v", err)
+	}
+	err = device.discardStale()
+	if err != nil {
+		t.Errorf("AbacoUDP.discardStale() failed: %v", err)
+	}
+	go func() { <-device.data }()
+	err = device.stop()
+	if err != nil {
+		t.Errorf("AbacoUDP.stop() failed: %v", err)
+	}
+}
+
+func TestAbacoSource(t *testing.T) {
+	source, err := NewAbacoSource()
+	if err != nil {
+		t.Fatalf("NewAbacoSource() fails: %s", err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	cardnum := -rand.Intn(99998) - 1 // Rand # between -1 and -99999
+
+	dev, err := NewAbacoRing(cardnum)
+	if err != nil {
+		t.Fatalf("NewAbacoRing(%d) fails: %s", cardnum, err)
+	}
+	source.arings[cardnum] = dev
+	source.Nrings++
+
+	var config AbacoSourceConfig
+
+	// Check that ActiveCards and HostPortUDP slices are unique-ified when source.Configure(&config) called.
+	config.ActiveCards = []int{4, 3, 3, 3, 3, 3, 4, 3, 3}
+	config.HostPortUDP = []string{"localhost:4444", "localhost:3333", "localhost:4444"}
+	err = source.Configure(&config)
+	if err == nil {
+		t.Fatalf("AbacoSource.Configure(%v) should fail but doesn't", config)
+	}
+	if len(config.ActiveCards) > 2 {
+		t.Errorf("AbacoSource.Configure() returns with repeats in ActiveCards=%v", config.ActiveCards)
+	}
+	if len(config.HostPortUDP) > 2 {
+		t.Errorf("AbacoSource.Configure() returns with repeats in HostPortUDP=%v", config.HostPortUDP)
+	}
+
+	deviceCodes := []int{cardnum}
+	config.ActiveCards = deviceCodes
+	config.HostPortUDP = []string{}
+	err = source.Configure(&config)
+	if err != nil {
+		t.Fatalf("AbacoSource.Configure(%v) fails: %s", config, err)
+	}
+
+	ringname := fmt.Sprintf("xdma%d_c2h_0_buffer", cardnum)
+	ringdesc := fmt.Sprintf("xdma%d_c2h_0_description", cardnum)
+	rb, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
+	if err != nil {
+		t.Fatalf("Could not open ringbuffer: %s", err)
+	}
+	defer rb.Unlink()
+	const packetAlign = 8192
+	if err = rb.Create(256 * packetAlign); err != nil {
+		t.Fatalf("Failed RingBuffer.Create: %s", err)
+	}
+
+	const Nchan = 8
+	const Nsamp = 20000
+	const stride = 500 // We'll put this many samples into a packet
+	if stride*Nchan*2 > 8000 {
+		t.Fatalf("Packet payload size %d exceeds 8000 bytes", stride*Nchan*2)
+	}
+
+	abortSupply := make(chan interface{})
+	supplyDataForever := func() {
+		p := packets.NewPacket(10, 20, 100, 0)
+		d := make([]int16, Nchan*Nsamp)
+		for i := 0; i < Nchan; i++ {
+			freq := (float64(i + 2)) / float64(Nsamp)
+			for j := 0; j < Nsamp; j++ {
+				d[i+Nchan*j] = int16(30000.0 * math.Cos(freq*float64(j)))
+			}
+		}
+
+		empty := make([]byte, packetAlign)
+		dims := []int16{Nchan}
+		timer := time.NewTicker(10 * time.Millisecond)
+		packetcount := 0
+		for {
+			select {
+			case <-abortSupply:
+				return
+			case <-timer.C:
+				for i := 0; i < Nsamp; i += stride {
+					p.NewData(d[i:i+stride*Nchan], dims)
+					// Skip 2 packets of every 80, the 1st and 80th.
+					packetcount++
+					if packetcount%80 < 2 {
+						continue
+					}
+					b := p.Bytes()
+					b = append(b, empty[:packetAlign-len(b)]...)
+					if rb.BytesWriteable() >= len(b) {
+						rb.Write(b)
+					}
+				}
+			}
+		}
+	}
+	go supplyDataForever()
+
+	queuedRequests := make(chan func())
+	Npresamp := 256
+	Nsamples := 1024
+	if err := Start(source, queuedRequests, Npresamp, Nsamples); err != nil {
+		fmt.Printf("Result of Start(source,...): %s\n", err)
+		t.Fatal(err)
+	}
+	if source.nchan != Nchan {
+		t.Errorf("source.nchan=%d, want %d", source.nchan, Nchan)
+	}
+	time.Sleep(250 * time.Millisecond)
+	source.Stop()
+	source.RunDoneWait()
+
+	// Start a 2nd time
+	err = source.Configure(&config)
+	if err != nil {
+		t.Fatalf("AbacoSource.Configure(%v) fails: %s", config, err)
+	}
+	if err := Start(source, queuedRequests, Npresamp, Nsamples); err != nil {
+		fmt.Printf("Result of Start(source,...): %s\n", err)
+		t.Fatal(err)
+	}
+	time.Sleep(50 * time.Millisecond)
+	source.Stop()
+	close(abortSupply)
+	source.RunDoneWait()
+}
+
+func prepareDemux(nframes int) (*AbacoGroup, []*packets.Packet, [][]RawType) {
+	const offset = 1
+	const nchan = 16
+	const enable = true
+	const resetAfter = 20000
+	const bias = false
+	const pulseSign = +1
+	group := NewAbacoGroup(GroupIndex{Firstchan: offset, Nchan: nchan}, enable, bias, resetAfter, pulseSign)
+	group.unwrap = group.unwrap[:0] // get rid of phase unwrapping
+
+	copies := make([][]RawType, nchan)
+	for i := 0; i < nchan; i++ {
+		copies[i] = make([]RawType, nframes)
+	}
+	const stride = 4096 / nchan
+	dims := []int16{nchan}
+	allpackets := make([]*packets.Packet, 0)
+	for j := 0; j < nframes; j += stride {
+		p := packets.NewPacket(10, 20, uint32(j/stride), offset)
+		d := make([]int16, 0, 4096)
+		for k := 0; k < stride; k++ {
+			for m := 0; m < nchan; m++ {
+				d = append(d, int16(m+j+k))
+			}
+		}
+		p.NewData(d, dims)
+		allpackets = append(allpackets, p)
+	}
+	return group, allpackets, copies
+}
+
+func TestDemux(t *testing.T) {
+	const nframes = 32768
+	group, allpackets, copies := prepareDemux(nframes)
+	want := (nframes * len(copies)) / 4096
+	if len(allpackets) != want {
+		t.Errorf("prepareDemux returns %d packets, want %d", len(allpackets), want)
+	}
+
+	group.queue = append(group.queue, allpackets...)
+	group.demuxData(copies, nframes)
+	for chidx, datacopy := range copies {
+		for i, val := range datacopy {
+			want := RawType(i + chidx)
+			if val != want {
+				t.Fatalf("datacopies[%d][%d] =  0x%x, want 0x%x", chidx, i, val, want)
+			}
+		}
+	}
+}
+
+func BenchmarkDemux(b *testing.B) {
+	const nframes = 32768
+	group, allpackets, copies := prepareDemux(nframes)
+	for i := 0; i < b.N; i++ {
+		group.queue = append(group.queue, allpackets...)
+		group.demuxData(copies, nframes)
+	}
+}
---
+++ lancero_test.go
@@ -0,0 +1,383 @@
+package dastard
+
+import (
+	"testing"
+	"time"
+
+	"github.com/usnistgov/dastard/lancero"
+)
+
+// TestChannelOrder checks the map from channel to readout numbering
+func TestChannelOrder(t *testing.T) {
+	ls, err := NewLanceroSource()
+	if err != nil {
+		t.Error("NewLanceroSource failed:", err)
+	}
+	d0 := LanceroDevice{devnum: 0, nrows: 4, ncols: 4, cardDelay: 1}
+	d1 := LanceroDevice{devnum: 1, nrows: 3, ncols: 3, cardDelay: 1}
+	ls.devices[0] = &d0
+	ls.devices[1] = &d1
+	config := LanceroSourceConfig{FiberMask: 0xbeef, ActiveCards: []int{0, 1, 2},
+		CardDelay: []int{1, 1, 1}}
+	ls.Configure(&config)
+	// Configure reads nrows from cringeGlobals.json, to test chan2readoutOrder we need to set nrows back to the original values
+	ls.devices[0].nrows = 4
+	ls.devices[1].nrows = 3
+
+	if len(ls.active) > 2 {
+		t.Errorf("ls.active contains %d cards, want 2", len(ls.active))
+	}
+	ls.updateChanOrderMap()
+	expect := []int{
+		0, 1, 8, 9, 16, 17, 24, 25,
+		2, 3, 10, 11, 18, 19, 26, 27,
+		4, 5, 12, 13, 20, 21, 28, 29,
+		6, 7, 14, 15, 22, 23, 30, 31,
+		32, 33, 38, 39, 44, 45,
+		34, 35, 40, 41, 46, 47,
+		36, 37, 42, 43, 48, 49,
+	}
+	for i, v := range ls.chan2readoutOrder {
+		if v != expect[i] {
+			t.Errorf("chan2readoutOrder[%d]=%d, want %d", i, v, expect[i])
+		}
+	}
+
+	config.ActiveCards = []int{0}
+	ls.Configure(&config)
+	// Configure reads nrows from cringeGlobals.json, to test chan2readoutOrder we need to set nrows back to the original values
+	ls.devices[0].nrows = 4
+	ls.devices[1].nrows = 3
+	ls.updateChanOrderMap()
+	for i, v := range ls.chan2readoutOrder {
+		if v != expect[i] {
+			t.Errorf("chan2readoutOrder[%d]=%d, want %d", i, v, expect[i])
+		}
+	}
+
+	config.ActiveCards = []int{1}
+	ls.Configure(&config)
+	// Configure reads nrows from cringeGlobals.json, to test chan2readoutOrder we need to set nrows back to the original values
+	ls.devices[0].nrows = 4
+	ls.devices[1].nrows = 3
+	ls.updateChanOrderMap()
+	for i, v := range ls.chan2readoutOrder {
+		if v != expect[i+32]-32 {
+			t.Errorf("chan2readoutOrder[%d]=%d, want %d", i, v, expect[i+32]-32)
+		}
+	}
+
+	// test roundint
+	fs := []float64{-1.4, -.6, -.4, 0, .4, .5, .6, 1.4}
+	es := []int{-1, -1, 0, 0, 0, 1, 1, 1}
+	for i, f := range fs {
+		if es[i] != roundint(f) {
+			t.Errorf("roundint(%f)=%d, want %d", f, roundint(f), es[i])
+		}
+	}
+}
+
+func TestPrepareChannels(t *testing.T) {
+	ls, err := NewLanceroSource()
+	if err != nil {
+		t.Error("NewLanceroSource failed:", err)
+	}
+	d0 := LanceroDevice{devnum: 0, nrows: 4, ncols: 4, cardDelay: 1}
+	d1 := LanceroDevice{devnum: 1, nrows: 3, ncols: 3, cardDelay: 1}
+	ls.devices[0] = &d0
+	ls.devices[1] = &d1
+	ls.active = make([]*LanceroDevice, 2)
+	for i, device := range ls.devices {
+		ls.active[i] = device
+		ls.nchan += device.ncols * device.nrows * 2
+	}
+
+	// Test normal chan numbering
+	expect := make([]int, 0, 200)
+	ls.PrepareChannels()
+	for i := 0; i < ls.nchan/2; i++ {
+		expect = append(expect, i)
+		expect = append(expect, i)
+	}
+	for i := 0; i < len(expect); i++ {
+		if ls.chanNumbers[i] != expect[i] {
+			t.Errorf("channel number for index %d is %d, want %d", i, ls.chanNumbers[i], expect[i])
+		}
+	}
+
+	// Test chan numbering with FirstRow > 1
+	ls.firstRowChanNum = 10
+	ls.PrepareChannels()
+	for i := 0; i < len(expect); i++ {
+		expect[i] += ls.firstRowChanNum
+		if ls.chanNumbers[i] != expect[i] {
+			t.Errorf("channel number for index %d is %d, want %d", i, ls.chanNumbers[i], expect[i])
+		}
+	}
+
+	// Test chan numbering with chanSepCards > 1
+	ls.firstRowChanNum = 1
+	ls.chanSepCards = 1000
+	ls.PrepareChannels()
+	expect = expect[:0] // truncate
+	for i := 0; i < ls.nchan/2; i++ {
+		expect = append(expect, i)
+		expect = append(expect, i)
+	}
+	for i := 2 * d0.nrows * d0.ncols; i < ls.nchan; i++ {
+		expect[i] += ls.chanSepCards - d0.nrows*d0.ncols
+	}
+	for i := 0; i < len(expect); i++ {
+		expect[i] += ls.firstRowChanNum
+		if ls.chanNumbers[i] != expect[i] {
+			t.Errorf("channel number for index %d is %d, want %d", i, ls.chanNumbers[i], expect[i])
+		}
+	}
+
+	// Test chan numbering with chanSepCols > 1
+	ls.firstRowChanNum = 1
+	ls.chanSepCards = 0
+	ls.chanSepColumns = 100
+	ls.PrepareChannels()
+	expect = expect[:0] // truncate
+	for c := 0; c < d0.ncols; c++ {
+		for r := 0; r < d0.ncols; r++ {
+			expect = append(expect, r+c*ls.chanSepColumns)
+			expect = append(expect, r+c*ls.chanSepColumns)
+		}
+	}
+	for c := d0.ncols; c < d0.ncols+d1.ncols; c++ {
+		for r := 0; r < d1.ncols; r++ {
+			expect = append(expect, r+c*ls.chanSepColumns)
+			expect = append(expect, r+c*ls.chanSepColumns)
+		}
+	}
+	for i := 0; i < len(expect); i++ {
+		expect[i] += ls.firstRowChanNum
+		if ls.chanNumbers[i] != expect[i] {
+			t.Errorf("channel number for index %d is %d, want %d", i, ls.chanNumbers[i], expect[i])
+		}
+	}
+
+	// Test chan numbering with chanSepCols > 1, chanSepColumns > 1
+	ls.firstRowChanNum = 1
+	ls.chanSepCards = 1000
+	ls.chanSepColumns = 100
+	ls.PrepareChannels()
+	expect = expect[:0] // truncate
+	for c := 0; c < d0.ncols; c++ {
+		for r := 0; r < d0.ncols; r++ {
+			expect = append(expect, r+c*ls.chanSepColumns)
+			expect = append(expect, r+c*ls.chanSepColumns)
+		}
+	}
+	for c := 0; c < d1.ncols; c++ {
+		for r := 0; r < d1.ncols; r++ {
+			expect = append(expect, r+c*ls.chanSepColumns+ls.chanSepCards)
+			expect = append(expect, r+c*ls.chanSepColumns+ls.chanSepCards)
+		}
+	}
+	for i := 0; i < len(expect); i++ {
+		expect[i] += ls.firstRowChanNum
+		if ls.chanNumbers[i] != expect[i] {
+			t.Errorf("channel number for index %d is %d, want %d", i, ls.chanNumbers[i], expect[i])
+		}
+	}
+
+	// Make sure PrepareChannels also correctly checking chanSepCards/Columns
+	type Checks struct {
+		sepCards, sepColumns int
+		expect               bool
+	}
+	checks := []Checks{
+		Checks{0, 0, true},
+		Checks{0, 2, false},
+		Checks{0, 3, false},
+		Checks{0, 4, true},
+		Checks{100, 0, true},
+		Checks{16, 0, true},
+		Checks{15, 0, false},
+		Checks{16, 4, true},
+		Checks{19, 4, true},
+		Checks{19, 5, false},
+		Checks{20, 5, true},
+		Checks{-1, 0, false},
+		Checks{0, -1, false},
+	}
+	for _, chk := range checks {
+		ls.chanSepCards = chk.sepCards
+		ls.chanSepColumns = chk.sepColumns
+		err := ls.PrepareChannels()
+		succeed := err == nil
+		if succeed != chk.expect {
+			if chk.expect {
+				t.Errorf("LanceroSource.PrepareChannels() should succeed with sepCards=%d, sepColumns=%d, but returned %v",
+					chk.sepCards, chk.sepColumns, err)
+			} else {
+				t.Errorf("LanceroSource.PrepareChannels() should fail with sepCards=%d, sepColumns=%d, but didn't",
+					chk.sepCards, chk.sepColumns)
+			}
+		}
+
+	}
+}
+
+func TestNoHardwareSource(t *testing.T) {
+	var ncolsSet, nrowsSet, linePeriodSet, nLancero int
+	ncolsSet = 1
+	nrowsSet = 4
+	linePeriodSet = 1000 // don't make this too low, or test -race will fail
+	nLancero = 3
+	source := new(LanceroSource)
+	source.readPeriod = time.Millisecond
+	source.devices = make(map[int]*LanceroDevice, nLancero)
+	source.buffersChan = make(chan BuffersChanType, 25)
+	cardDelay := []int{0} // a single card delay value works for multiple cards
+	activeCards := make([]int, nLancero)
+	for i := 0; i < nLancero; i++ {
+		lan, err := lancero.NewNoHardware(ncolsSet, nrowsSet, linePeriodSet)
+		if err != nil {
+			t.Error(err)
+		}
+		dev := LanceroDevice{card: lan, devnum: i}
+		source.devices[i] = &dev
+		activeCards[i] = i
+		source.ncards++
+	}
+	// above is essentially NewLanceroSource
+
+	config := LanceroSourceConfig{CardDelay: cardDelay,
+		ActiveCards: make([]int, nLancero)}
+	if err := source.Configure(&config); err == nil && nLancero > 1 {
+		t.Error("expected error for re-using a device")
+	}
+	config = LanceroSourceConfig{CardDelay: cardDelay,
+		ActiveCards: activeCards, FirstRow: 1}
+	if err := source.Configure(&config); err != nil {
+		t.Error(err)
+	}
+	for i := 0; i < nLancero; i++ {
+		if config.ActiveCards[i] != config.DastardOutput.AvailableCards[i] {
+			t.Errorf("AvailableCards not populated correctly. AvailableCards %v", config.DastardOutput.AvailableCards)
+		}
+	}
+
+	// Start will call sampleCard
+	// sampleCard will calculate nrows and lsync
+	// if nrows doesn't match, it will error
+	// if lsync doesn't match, it will give a warning
+	// since these values from from cringeGlobals.json, we need to manully set them to match before this test
+	for _, dev := range source.devices {
+		dev.nrows = nrowsSet
+		dev.lsync = linePeriodSet // we we calculate different values for lsync here, don't freak out over warnings
+	}
+	if err := Start(source, nil, 256, 1024); err != nil {
+		//source.Stop() // source.Stop should work here, but it fails and we don't see the error message from t.Fatal
+		t.Fatal(err)
+	}
+	defer source.Stop()
+
+	if source.chanNumbers[3] != 2 {
+		t.Errorf("LanceroSource.chanNumbers[3] has %v, want 2", source.chanNumbers[3])
+	}
+	if source.chanNames[3] != "chan2" {
+		t.Errorf("LanceroSource.chanNames[3] has %v, want chan2", source.chanNames[3])
+	}
+	if source.chanNames[2] != "err2" {
+		t.Errorf("LanceroSource.chanNames[2] %v, want err2", source.chanNames[3])
+	}
+	mfo := MixFractionObject{ChannelIndices: []int{0}, MixFractions: []float64{1.0}}
+	if _, err := source.ConfigureMixFraction(&mfo); err == nil {
+		t.Error("expected error for mixing on even channel")
+	}
+	mfo.ChannelIndices[0] = 1
+	mix, err := source.ConfigureMixFraction(&mfo)
+	if err != nil {
+		t.Error(err)
+	} else if mix[1] != 1.0 {
+		t.Errorf("source.ConfigureMixFraction returns [%f], want %f", mix[0], 1.0)
+	}
+	time.Sleep(20 * time.Millisecond) // wait long enough for some data to be processed
+	// these tests get lsync right at first, but while data is being processed
+	// the lsync is not right. about half the time I run the test there are 3 blocking reads
+	// at which point the lancero source shuts down due to it seeing lsync change
+	// then Stop() will error but the err value is not checked so it doesn't cause a test failure
+}
+
+func TestMix(t *testing.T) {
+	data := make([]RawType, 10)
+	errData := make([]RawType, len(data))
+	for i := range errData {
+		errData[i] = RawType(i * 4)
+
+	}
+	mix := Mix{errorScale: 0}
+	mix.MixRetardFb(&data, &errData)
+	passed := true
+	for i := range data {
+		if data[i] != 0 {
+			passed = false
+		}
+	}
+	if !passed {
+		t.Errorf("want all zeros, have\n%v", data)
+	}
+	mix = Mix{errorScale: 1}
+	mix.MixRetardFb(&data, &errData)
+	passed = true
+	for i := range data {
+		if data[i] != errData[i] {
+			passed = false
+		}
+	}
+	if !passed {
+		t.Errorf("want %v\n have\n%v", errData, data)
+	}
+	if mix.lastFb != 0 {
+		t.Errorf("want 0, have %v", mix.lastFb)
+	}
+	mix.lastFb = 100
+	mix.MixRetardFb(&data, &errData)
+	passed = true
+	expect := []RawType{100 + 0, (0 + 1) * 4, (1 + 2) * 4, (2 + 3) * 4,
+		(3 + 4) * 4, (4 + 5) * 4, (5 + 6) * 4, (6 + 7) * 4, (7 + 8) * 4, (8 + 9) * 4}
+	for i := range data {
+		if data[i] != expect[i] {
+			passed = false
+		}
+	}
+	if !passed {
+		t.Errorf("want %v\nhave\n%v", expect, data)
+	}
+	if mix.lastFb != 36 {
+		t.Errorf("want 36, have %v", mix.lastFb)
+	}
+	// test 2 LSBs of feedback are masked off
+	for i := range data {
+		data[i] = RawType(i % 4)
+	}
+	mix = Mix{errorScale: 0}
+	mix.MixRetardFb(&data, &errData)
+	passed = true
+	for i := range data {
+		if data[i] != 0 {
+			passed = false
+		}
+	}
+	if !passed {
+		t.Errorf("have %v, want all zeros", data)
+	}
+
+	// Check that overflows are clamped to proper range
+	err1 := []RawType{100, 0, 65436} // {100, 0, -100} as signed ints
+	mix = Mix{errorScale: 1.0}
+	mix.lastFb = 65530
+	fb := []RawType{0, 50, 50}
+	expect = []RawType{65535, 0, 0}
+	mix.MixRetardFb(&fb, &err1)
+	for i, e := range expect {
+		if fb[i] != e {
+			t.Errorf("mix with overflows fb[%d]=%d, want %d", i, fb[i], e)
+		}
+	}
+}
