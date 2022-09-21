968e12cc4e163ed6335e8c25b64af8ca7e6e79e0
Joe Fowler <drjoefowler@gmail.com>
2022-09-21 14:35:07 -0600

    Squashed commit of the following:
    commit 0ae1d50c9eadac3625ed87e91d634d819a609620
    Author: Joe Fowler <drjoefowler@gmail.com>
    Date:   Wed Sep 21 14:32:11 2022 -0600
    
        Fix bahama: wasn't sending packets!
    
    commit 761d2a784ddc0002998085ef8d9ed938e22f99e8
    Author: Joe Fowler <drjoefowler@gmail.com>
    Date:   Wed Sep 21 12:12:08 2022 -0600
    
        Pass around pointers to slices, not slices
    
        Use pointers to slices in the `sync.Pool` `AbacoUDPReceiver.start`, rather than slices (`[]byte`) themselves. VSCode says this is a best practice. I tried it in 5900435 but got the allocator function wrong.
    
    commit e9517a47c8e9a364a54867355c54ce363db6b3d2
    Author: Joe Fowler <drjoefowler@gmail.com>
    Date:   Wed Sep 21 14:23:22 2022 -0600
    
        Update .gitignore
    
    commit 8b46ddfaa1bf25a4a6d9e3531dada638b309006c
    Author: Joe Fowler <drjoefowler@gmail.com>
    Date:   Wed Sep 21 14:20:45 2022 -0600
    
        Add a benchmark to bahama_test

--- cmd/bahama/bahama.go
+++ cmd/bahama/bahama.go
@@ -24,7 +24,7 @@ func clearRings(nclear int) error {
 		ringdesc := fmt.Sprintf("xdma%d_c2h_0_description", cardnum)
 		ring, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
 		if err != nil {
-			return fmt.Errorf("Could not open ringbuffer: %s", err)
+			return fmt.Errorf("could not open ringbuffer: %s", err)
 		}
 		ring.Unlink() // in case it exists from before
 	}
@@ -161,7 +161,7 @@ func generateData(Nchan, firstchanOffset int, packetchan chan []byte, cancel cha
 	stride := 4000 / Nchan // We'll put this many samples into each packet
 	valuesPerPacket := stride * Nchan
 	if 2*valuesPerPacket > 8000 {
-		return fmt.Errorf("Packet payload size %d exceeds 8000 bytes", 2*valuesPerPacket)
+		return fmt.Errorf("packet payload size %d exceeds 8000 bytes", 2*valuesPerPacket)
 	}
 	const counterRate = 1e8 // counts per second
 	countsPerSample := uint64(counterRate/sampleRate + 0.5)
@@ -285,11 +285,11 @@ func ringwriter(cardnum int, packetchan chan []byte, ringsize int) error {
 	ringdesc := fmt.Sprintf("xdma%d_c2h_0_description", cardnum)
 	ring, err := ringbuffer.NewRingBuffer(ringname, ringdesc)
 	if err != nil {
-		return fmt.Errorf("Could not open ringbuffer %d: %s", cardnum, err)
+		return fmt.Errorf("could not open ringbuffer %d: %s", cardnum, err)
 	}
 	ring.Unlink() // in case it exists from before
 	if err = ring.Create(ringsize); err != nil {
-		return fmt.Errorf("Failed RingBuffer.Create(%d): %s", ringsize, err)
+		return fmt.Errorf("failed RingBuffer.Create(%d): %s", ringsize, err)
 	}
 	fmt.Printf("Generating data in shm:%s\n", ringname)
 
@@ -434,7 +434,7 @@ func main() {
 		if control.udp {
 			portnum := (*port) + cardnum
 			if err := udpwriter(portnum, packetchan); err != nil {
-				fmt.Printf("udpwriter(%d,...) failed: %v\n", cardnum, err)
+				fmt.Printf("udpwriter(%d,...) failed: %v\n", portnum, err)
 				continue
 			}
 		} else {
