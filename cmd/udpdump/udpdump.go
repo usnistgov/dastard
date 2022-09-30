d0aaa1fd5b4e2bef021e09c8bf15a5de54f07aca
Joe Fowler <drjoefowler@gmail.com>
2022-09-30 07:35:30 -0600

    Default to localhost

--- cmd/udpdump/udpdump.go
+++ cmd/udpdump/udpdump.go
@@ -11,12 +11,14 @@ import (
 )
 
 func probe(npack int, endpoint string) error {
+	fmt.Printf("Probing %s for the first %d packets received...\n", endpoint, npack)
 	address, err := net.ResolveUDPAddr("udp", endpoint)
 	if err != nil {
 		return err
 	}
 	ServerConn, _ := net.ListenUDP("udp", address)
 	defer ServerConn.Close()
+
 	buf := make([]byte, 8192)
 	for i := 0; i < npack; i++ {
 		if _, _, err := ServerConn.ReadFromUDP(buf); err != nil {
@@ -29,7 +31,6 @@ func probe(npack int, endpoint string) error {
 			fmt.Printf("%s with %d frames for %d channels [%d-%d]\n", pack.String(),
 				pack.Frames(), nchan, chan0, chan0+nchan-1)
 		}
-		// fmt.Println("Received ", string(buf[0:n]), " from ", addr)
 	}
 	return nil
 }
@@ -37,15 +38,17 @@ func probe(npack int, endpoint string) error {
 func main() {
 	var npack int
 	var port int
-	host := "localhost"
+	const default_host = "localhost"
 	const default_port = 4000
+    host := default_host
 	flag.IntVar(&npack, "n", 10, "Number of packets to dump")
 	flag.IntVar(&port, "port", default_port, "Port to monitor")
 	flag.IntVar(&port, "p", default_port, "Port to monitor (shorthand)")
 
 	flag.Usage = func() {
-		fmt.Println("udpdump, for dumping the first N packets' headers")
-		fmt.Println("Usage: udpdump [flags] host[:port]")
+		fmt.Printf("udpdump, for dumping the first N packet headers, by default those from localhost:%d\n",
+			default_port)
+		fmt.Println("Usage: udpdump [flags] [host][:port]")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -58,22 +61,25 @@ func main() {
 				fmt.Printf("Cannot parse host '%s' with %d colon separators\n", host, len(pieces)-1)
 				return
 			}
-			if attachedport, err := strconv.Atoi(pieces[1]); err != nil {
+			attachedport, err := strconv.Atoi(pieces[1])
+            if err != nil {
 				fmt.Printf("Cannot convert port '%s' to integer\n", pieces[1])
 				return
-			} else {
-				if port != default_port && port != attachedport {
-					fmt.Printf("Cannot use -p argument and a conflicting host:port pair\n")
-					return
-				}
-				host = pieces[0]
-				port = attachedport
 			}
+			if port != default_port && port != attachedport {
+				fmt.Printf("Cannot use -p argument and a conflicting host:port pair\n")
+				return
+			}
+			if len(pieces[0]) == 0 {
+                host = default_host
+            } else {
+				host = pieces[0]
+			}
+			port = attachedport
 		}
 	}
 
 	endpoint := fmt.Sprintf("%s:%4.4d", host, port)
-	fmt.Printf("Probing %s for first %d packets\n", endpoint, npack)
 	if err := probe(npack, endpoint); err != nil {
 		fmt.Printf("error: %v\n", err)
 	}
