e059e355f3d2bc80509cfd662c923a2527bc6895
Joe Fowler <drjoefowler@gmail.com>
2023-03-08 11:05:07 -0700

    Note to future self: remove the nodb build tag?

--- internal/mysql/nodb_mysql.go
+++ internal/mysql/nodb_mysql.go
@@ -5,6 +5,12 @@
 // build time with the "-tags nodb" build tag (or by using the Makefile as
 // "NODB=true make build"). It puts no-ops in place of the usual communication with a
 // MySQL server.
+//
+// TODO: This entire conditional compilation might be pointless, because it seems
+// that packages database/sql and github.com/go-sql-driver/mysql have no library
+// dependencies. They just use the regular net package. In that case, compiling in the
+// regular mysql.go file might have almost zero impact on the dependencies.
+// Consider removing (8 March 2023, JWF)?
 
 package mysql
 
