19a9cfae21367bbbd1114d57eb0d1ef24faf415d
Joe Fowler <drjoefowler@gmail.com>
2024-05-03 18:12:50 -0600

    Change Flush to be a blocking call
    (That's the behavior of `bufio.Writer`, so we expect that here.)

--- asyncbufio/asyncbufio.go
+++ asyncbufio/asyncbufio.go
@@ -11,9 +11,10 @@ import (
 type Writer struct {
 	writer        *bufio.Writer  // Buffered writer: this does the writing
 	flushNow      chan struct{}  // Channel to signal the underlying writer to flush itself
+	flushComplete chan struct{}  // Channel to signal underlying writer flush is complete
 	databuffer    chan []byte    // Channel to hold data before writing it
 	flushInterval time.Duration  // Interval for flushing the writer periodically
-	wg            sync.WaitGroup // WaitGroup to synchronize end of writing
+	doneWriting   sync.WaitGroup // WaitGroup to synchronize end of writing
 }
 
 // NewWriter creates a new Writer instance.
@@ -22,10 +23,11 @@ func NewWriter(w io.Writer, bufferSize int, flushInterval time.Duration) *Writer
 		writer:        bufio.NewWriter(w),
 		databuffer:    make(chan []byte, bufferSize),
 		flushNow:      make(chan struct{}),
+		flushComplete: make(chan struct{}),
 		flushInterval: flushInterval, // Set the flush interval
 	}
 
-	aw.wg.Add(1)
+	aw.doneWriting.Add(1)
 	go aw.writeLoop()
 	return aw
 }
@@ -41,8 +43,10 @@ func (aw *Writer) Write(p []byte) (int, error) {
 }
 
 // Flush flushes any remaining data in the buffer to the underlying writer.
+// Blocks until the flush is complete.
 func (aw *Writer) Flush() error {
 	aw.flushNow <- struct{}{}
+	<-aw.flushComplete
 	return nil
 }
 
@@ -50,13 +54,13 @@ func (aw *Writer) Flush() error {
 // It is possible to cause a panic by calling Write(p) or Flush() after Close()--we don't
 // test for that case.
 func (aw *Writer) Close() {
-	close(aw.flushNow) // Closing the flushNow channel signals the writeLoop to exit
-	aw.wg.Wait()       // Wait until writing is complete
+	close(aw.flushNow)    // Closing the flushNow channel signals the writeLoop to exit
+	aw.doneWriting.Wait() // Wait until writing is complete
 }
 
 // writeLoop is a goroutine that continuously moves data from the buffer to the writer.
 func (aw *Writer) writeLoop() {
-	defer aw.wg.Done() // Decrement WaitGroup when writing is complete
+	defer aw.doneWriting.Done() // Decrement WaitGroup when writing is complete
 
 	ticker := time.NewTicker(aw.flushInterval) // Ticker to flush periodically
 	defer ticker.Stop()                        // Stop the ticker when the writeLoop exits
@@ -70,6 +74,9 @@ func (aw *Writer) writeLoop() {
 		case _, ok := <-aw.flushNow:
 			breakFromLoop = !ok
 			aw.flush()
+			// Signal whoever requested this that flushing is done
+			aw.flushComplete <- struct{}{}
+
 		case <-ticker.C:
 			aw.flush()
 		}
@@ -80,12 +87,14 @@ func (aw *Writer) writeLoop() {
 }
 
 func (aw *Writer) flush() {
-	defer aw.writer.Flush()
+	// This loop empties the aw.databuffer channel before finally
+	// calling the underlying writer's Flush() method
 	for {
 		select {
 		case data := <-aw.databuffer:
 			aw.writer.Write(data)
 		default:
+			aw.writer.Flush()
 			return
 		}
 	}
