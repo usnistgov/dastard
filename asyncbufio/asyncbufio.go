b614c4725d14f94beb2b7e28d5fba08e36c03035
Joe Fowler <drjoefowler@gmail.com>
2024-05-25 11:32:58 -0600

    More careful with naming `asyncbufio` internals

--- asyncbufio/asyncbufio.go
+++ asyncbufio/asyncbufio.go
@@ -11,15 +11,15 @@ type Writer struct {
 	writer        *bufio.Writer // Buffered writer: this does the writing
 	flushNow      chan struct{} // Channel to signal the underlying writer to flush itself
 	flushComplete chan struct{} // Channel to signal underlying writer flush is complete
-	databuffer    chan []byte   // Channel to hold data before writing it
+	datachannel   chan []byte   // Channel to hold data before writing it
 	flushInterval time.Duration // Interval for flushing the writer periodically
 }
 
 // NewWriter creates a new Writer instance.
-func NewWriter(w io.Writer, bufferSize int, flushInterval time.Duration) *Writer {
+func NewWriter(w io.Writer, channelDepth int, flushInterval time.Duration) *Writer {
 	aw := &Writer{
 		writer:        bufio.NewWriter(w),
-		databuffer:    make(chan []byte, bufferSize),
+		datachannel:   make(chan []byte, channelDepth),
 		flushNow:      make(chan struct{}),
 		flushComplete: make(chan struct{}),
 		flushInterval: flushInterval, // Set the flush interval
@@ -29,22 +29,22 @@ func NewWriter(w io.Writer, bufferSize int, flushInterval time.Duration) *Writer
 	return aw
 }
 
-// Write stores data to the Writer's buffer for later writing.
+// Write sends data to the Writer's channel, storing it for later writing.
 func (aw *Writer) Write(p []byte) (int, error) {
 	select {
-	case aw.databuffer <- p:
+	case aw.datachannel <- p:
 		return len(p), nil
 	default:
-		return 0, io.ErrShortWrite // Return an error if buffer is full
+		return 0, io.ErrShortWrite // Return an error if channel is full
 	}
 }
 
-// WriteString writes a string to the buffer for later writing (with an annoying copy--sorry!)
+// WriteString sends a string to the channel for later writing (with an annoying copy--sorry!)
 func (aw *Writer) WriteString(s string) (int, error) {
 	return aw.Write([]byte(s))
 }
 
-// Flush flushes any remaining data in the buffer to the underlying writer.
+// Flush flushes any remaining data in the channel to the underlying writer.
 // Blocks until the flush is complete.
 func (aw *Writer) Flush() error {
 	aw.flushNow <- struct{}{}
@@ -53,22 +53,22 @@ func (aw *Writer) Flush() error {
 }
 
 // Close closes the Writer, flushing remaining data and waiting for the writeLoop to finish.
-// It is possible to cause a panic by calling Write(p) or Flush() after Close()--we don't
+// It will cause a panic to call Write(p) or Flush() after Close()--we don't
 // test for that case.
 func (aw *Writer) Close() {
 	close(aw.flushNow) // Closing the flushNow channel signals the writeLoop to exit
 	<-aw.flushComplete // Wait until writing is complete
 }
 
-// writeLoop is a goroutine that continuously moves data from the buffer to the writer.
+// writeLoop is a goroutine that continuously moves data from the channel to the writer.
 func (aw *Writer) writeLoop() {
 	ticker := time.NewTicker(aw.flushInterval) // Ticker to flush periodically
 	defer ticker.Stop()                        // Stop the ticker when the writeLoop exits
 
 	for {
 		select {
-		case data := <-aw.databuffer:
-			aw.writer.Write(data) // Write data from the buffer to the writer
+		case data := <-aw.datachannel:
+			aw.writer.Write(data) // Write data from the channel to the writer
 
 		case _, ok := <-aw.flushNow:
 			aw.flush()
@@ -85,11 +85,11 @@ func (aw *Writer) writeLoop() {
 }
 
 func (aw *Writer) flush() {
-	// This loop empties the aw.databuffer channel before finally
+	// This loop empties the aw.datachannel channel before finally
 	// calling the underlying writer's Flush() method
 	for {
 		select {
-		case data := <-aw.databuffer:
+		case data := <-aw.datachannel:
 			aw.writer.Write(data)
 		default:
 			aw.writer.Flush()
