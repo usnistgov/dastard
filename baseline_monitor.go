1fb000e2b8f28afc9bcf3a29d06163c53dec4a9f
Joe Fowler <drjoefowler@gmail.com>
2026-05-18 08:56:55 -0600

    Be more idiomatic; allow > 2^16 value averaging

--- baseline_monitor.go
+++ baseline_monitor.go
@@ -12,7 +12,7 @@ type BaselineMonitor struct {
 	chanNumber int
 	nAverage   int // how many samples to average
 	avgCounter int
-	avgSum     uint32
+	avgSum     uint64
 	nStore     int // how many averages to queue for analysis
 	nPeak      int // how many of the queued averages define the peak; should be 10-20% of nStore
 	averages   []float32
@@ -21,41 +21,44 @@ type BaselineMonitor struct {
 // NewBaselineMonitor creates and initializes a new BaselineMonitor.
 // Its behavior (average time, queue size) is fixed at creation time.
 func NewBaselineMonitor(chanNumber int, nAverage int, nStore int, nPeak int) *BaselineMonitor {
-	if nAverage > 65536 {
-		fmt.Printf("BaselineMonitor requires nAverage <= 65536, got %d\n", nAverage)
-		return nil
-	}
 	if nPeak > nStore {
 		fmt.Printf("BaselineMonitor requires nPeak < nStore, got %d, %d\n", nPeak, nStore)
 		return nil
 	}
 	averages := make([]float32, 0, nStore)
-	bmon := BaselineMonitor{
+	return &BaselineMonitor{
 		chanNumber: chanNumber,
 		nAverage:   nAverage,
 		nStore:     nStore,
 		nPeak:      nPeak,
 		averages:   averages,
 	}
-	return &bmon
 }
 
 func (bmon *BaselineMonitor) AddOneValue(v RawType) {
-	bmon.avgSum += uint32(v)
+	bmon.avgSum += uint64(v)
 	bmon.avgCounter += 1
 	if bmon.avgCounter >= bmon.nAverage {
 		bmon.performAverage()
 	}
 }
 
-func (bmon *BaselineMonitor) AddSliceValues(values []RawType) {
+func (bmon *BaselineMonitor) AddSliceValues(values []RawType) []*dastarddb.BaselineMonitorMessage {
+	var msgs []*dastarddb.BaselineMonitorMessage
+
 	for _, v := range values {
-		bmon.avgSum += uint32(v)
-		bmon.avgCounter += 1
+		bmon.avgSum += uint64(v)
+		bmon.avgCounter++
 		if bmon.avgCounter >= bmon.nAverage {
-			bmon.performAverage()
+			if thismsg := bmon.performAverage(); thismsg != nil {
+				if msgs == nil {
+					msgs = make([]*dastarddb.BaselineMonitorMessage, 0, 16)
+				}
+				msgs = append(msgs, thismsg)
+			}
 		}
 	}
+	return msgs
 }
 
 func (bmon *BaselineMonitor) performAverage() {
@@ -72,7 +75,7 @@ func (bmon *BaselineMonitor) analyzeQueue() {
 	slices.Sort(bmon.averages)
 	bestRange := bmon.averages[bmon.nPeak] - bmon.averages[0]
 	bestIdx := 0
-	for i := 0; i < bmon.nStore-bmon.nPeak; i++ {
+	for i := 1; i < bmon.nStore-bmon.nPeak; i++ {
 		thisrange := bmon.averages[bmon.nPeak+i] - bmon.averages[i]
 		if thisrange < bestRange {
 			bestRange = thisrange
@@ -80,8 +83,9 @@ func (bmon *BaselineMonitor) analyzeQueue() {
 		}
 	}
 	var sum float32
-	for i := bestIdx; i < bestIdx+bmon.nPeak; i++ {
-		sum += bmon.averages[i]
+	clusterValues := bmon.averages[bestIdx : bestIdx+bmon.nPeak]
+	for _, v := range clusterValues {
+		sum += v
 	}
 	baseline := sum / float32(bmon.nPeak)
 	bmon.averages = bmon.averages[:0]
